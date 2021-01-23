34269c30eab5fe13834e0bcae56704dcffa6df0d
mattrorvick <matthewrorvick@gmail.com>
2021-01-23 15:29:49 -0600

    Speed Converter km/h to mi/h

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        long mph = SpeedConverter.toMilesPerHour(-1);
+        System.out.println(mph);
+
+        SpeedConverter.printConversion(-1);
+
+    }
+
+}
